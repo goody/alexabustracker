ff1d41a590e085934825bab756ba3502ba4038b4
Scot Goodhart <scot.goodhart@abbott.com>
2016-05-10 22:33:20 -0500

    got promises working :)

--- myBusTracker.js
+++ myBusTracker.js
@@ -4,84 +4,93 @@ var busTracker = cta(process.env.API_KEY);
 var moment = require('moment-timezone');
 var _ = require('lodash');
 
-var fs = require('fs');
-
+/**
+ * 
+ * public functions
+ * 
+ */
  
- //TODO: returns an obj if there's only one bus and an array if there're more than one 
+ /**
+  * 
+  returns stop obj with buses approaching
+  //TODO: node_module returns an obj if there's only one bus and an array if there're more than one 
+  */
 function getStopSchedule(options) {
+    console.log('getStopSchedule', options);
     return new Promise(function (resolve, reject) {
-        var responseText = "I'm waiting for a response.";
         busTracker.predictionsByStop(options, function (err, data) {
             if (err) {
                 console.log('err:', err);
             }
-            if (data == null) {
-                responseText = 'There are no buses near.';
-            } else {
-                if (Array.isArray(data)) {
-                    var howManyBuses = data.length;
-                    var busTimes = '';
-                    for (var i = 0; i < howManyBuses; i++) {
-                        busTimes += _getArrivalTime(data[i].prdtm).toString();
-                        if(i < howManyBuses-1){
-                            busTimes += ' and ';
-                        }
-                    }
-                    responseText = 'The ' + data[0].rtdir + ' ' + data[0].rt + ' bus at ' + data[0].stpnm + ' has ' + howManyBuses + ' buses arriving in ' + busTimes + ' minutes';
-                } else {
-                    responseText = 'The ' + data.rtdir + ' ' + data.rt + ' bus at ' + data.stpnm + ' has one bus arriving in ' + _getArrivalTime(data.prdtm) + ' minutes.';
-                }
-            }
-            resolve(responseText);
-            reject(responseText);
+            resolve(data);
         });
     });
 }
+
+/**
+ * returns stop id from route number, direction, and stop by name
+ * 
+ */
+function getRouteStop(options) {
+    return new Promise(function (resolve, reject) {
+        var routeId = options.BusRouteNumber;
+        var routeDirection = options.RouteDirection;
+        var busStopName = options.BusStopName;
+
+        busTracker.stops(routeId, routeDirection, function (err, data) {
+            var result = {};
+            if (err) {
+                console.log('err', err);
+            }
+            if (data == null) {
+                result = null;
+            } else {
+                //filter results on cross street
+                var stop = _.filter(data, function (b) {
+                    return b.stpnm === busStopName.replace('and', '&');
+                });
+                console.log(stop);
+                result.stopIds = [stop[0].stpid];
+                console.log('test coming:');
+            }
+            resolve(result);
+        });
+    });
+}
+
+function renderBusText(responseData) {
+    var responseText = '';
+    if (responseData == null) {
+        responseText = 'There are no buses near.';
+    } else {
+        if (Array.isArray(responseData)) {
+            var howManyBuses = responseData.length;
+            var busTimes = '';
+            for (var i = 0; i < howManyBuses; i++) {
+                busTimes += _getArrivalTime(responseData[i].prdtm).toString();
+                if (i < howManyBuses - 1) {
+                    busTimes += ' and ';
+                }
+            }
+            responseText = 'The ' + responseData[0].rtdir + ' ' + responseData[0].rt + ' bus at ' + responseData[0].stpnm + ' has ' + howManyBuses + ' buses arriving in ' + busTimes + ' minutes';
+        } else {
+            responseText = 'The ' + responseData.rtdir + ' ' + responseData.rt + ' bus at ' + responseData.stpnm + ' has one bus arriving in ' + _getArrivalTime(responseData.prdtm) + ' minutes.';
+        }
+    }
+    return responseText;
+}
+
+/**
+ * 
+ * internal 
+ * 
+ */
 
 function _getArrivalTime(expectedTime){
     var expected = moment(expectedTime).tz("America/Chicago").format();
     var current = moment().tz("America/Chicago").format();
     var arriving = moment(expected).diff(current,'minutes');
     return arriving.toString();
-}
-
-function getRouteSchedule(options) {
-    return new Promise(function (resolve, reject) {
-        var responseText = '';
-        console.log('options', options);
-        //intent with route number, direction, and cross street
-        var routeId = options.BusRouteNumber;
-        var routeDirection = options.RouteDirection;
-        var busStopName = options.BusStopName;
-
-        busTracker.stops(routeId, routeDirection, function (err, data) {
-            if (err) {
-                console.dir(err);
-                reject(err);
-                // handle error
-            }
-            if (data == null) {
-                responseText = 'There are no buses near.';
-            }
-            //filter results on cross street
-            var test = _.filter(data, function (b) {
-                return b.stpnm === busStopName.replace('and', '&');
-            });
-            responseText = test;
-        });
-                // console.log('val3:', test);
-                // var options = { stopIds: [val[0].stpid] };
-                // getStopSchedule(options).then(function (val) {
-
-                //     console.log('val2: ', val);
-                //     responseText = val;
-                // })
-                //     .catch(function (err) {
-                //         console.log('err: ', err);
-                //         reject(err);
-                //     });
-        resolve(responseText);
-    });
 }
 
 /***
@@ -93,7 +102,7 @@ function getRouteSchedule(options) {
 //for local testing
 var options = {
     // // a list of up to 10 stop IDs 
-    // stopIds: [ "3766" ],
+    //stopIds: [ "3766" ],
     // // topCount is optional 
     // topCount: 5
     BusRouteNumber: 22,
@@ -101,16 +110,25 @@ var options = {
     BusStopName: 'Clark and Lawrence'
 };
 
+// getStopSchedule(options).then(function(val){
+//    console.log(renderBusText(val)); 
+// });
+getRouteStop(options)
+.then(getStopSchedule)
+.then(function(val){
+    console.log(renderBusText(val));
+});
 
 // for local testing
-getRouteSchedule(options).then(function(val){
-    console.log('val final: ', val);
-})
-.catch(function(err){
-    console.log('err: ', err);
-});
+// getRouteSchedule(options).then(function(val){
+//     console.log('val final: ', val);
+// })
+// .catch(function(err){
+//     console.log('err: ', err);
+// });
 
 module.exports = {
     getStopSchedule : getStopSchedule,
-    getRouteSchedule : getRouteSchedule
+    getRouteStop : getRouteStop,
+    renderBusText: renderBusText
 }
